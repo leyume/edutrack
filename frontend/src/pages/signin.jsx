5cdd11efae33339f5d23acb19f7c01d263621f2d
L eye <leyume@yahoo.com>
2023-09-13 18:06:33 +0100

    Added React Query and Firebase

--- frontend/src/pages/signin.jsx
+++ frontend/src/pages/signin.jsx
@@ -1,7 +1,34 @@
 import signbg from "/images/sign-bg.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { auth, signInWithEmailAndPassword } from "../config";
 
 export default function Signin() {
+  let navigate = useNavigate();
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+    const formData = Object.fromEntries(form.entries());
+
+    let { email, password } = formData;
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // console.log(user.accessToken);
+        localStorage.setItem("token", user.accessToken);
+        navigate("/dashboard");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error({ errorCode, errorMessage });
+      });
+  };
+
   return (
     <main className="w-full grid grid-cols-2 items-center justify-between">
       <div className="relative h-[calc(100vh-75px)] ">
@@ -17,21 +44,23 @@ export default function Signin() {
         </div>
       </div>
 
-      <form className="grid gap-4 px-10% pb-24">
+      <form className="grid gap-4 px-10% pb-24" onSubmit={loginHandler}>
         <h1 className="">Sign In</h1>
         <label className="grid gap-2">
           Email Address
-          <input type="text" className="p-2.5 rounded-md outline-none border border-#00000040" />
+          <input type="text" name="email" className="p-2.5 rounded-md outline-none border border-#00000040" />
         </label>
         <label className="grid gap-2">
           Password
-          <input type="password" className="p-2.5 rounded-md outline-none border border-#00000040" />
+          <input type="password" name="password" className="p-2.5 rounded-md outline-none border border-#00000040" />
         </label>
         <a href="" className="decoration-underline text-right">
           Forget your password?
         </a>
 
-        <button className="btn w-25%">Sign In</button>
+        <div>
+          <button className="btn">Sign In</button>
+        </div>
       </form>
     </main>
   );
